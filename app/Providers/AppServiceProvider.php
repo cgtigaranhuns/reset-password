a1ef60d69f334d02d4b97b4718d4767366278211
Fernando Silva <fernando.silva@garanhuns.ifpe.edu.br>
2026-09-17 15:41:01 -0300

    Configurar o rate limiter dedicado

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('password-reset', function (Request $request) {
+        return [
+            Limit::perHour(5)->by($request->ip()),
+            Limit::perHour(3)->by($request->input('enrollment')),
+        ];
+    });
     }
 }
